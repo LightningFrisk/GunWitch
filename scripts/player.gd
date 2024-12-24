205b0f67fb86e5c2de9b1319adb5355fd399760f
Caleb Cook <bmxcaleb@gmail.com>
2024-12-24 16:49:02 -0600

    animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,20 @@
 extends CharacterBody3D
 
+@onready var camera_mount = $cameraMount
+@onready var animation_player = $visuals/mixamo_base/AnimationPlayer
+@onready var visuals = $visuals
 
-const SPEED = 5.0
+var SPEED = 2.8
 const JUMP_VELOCITY = 4.5
 
-var sens_horizontal = .5
+var walking_speed = 3.0
+var running_speed = 5.0
+
+var running = false
+var is_locked = false
+
+@export var sens_horizontal = .35
+@export var sens_vertical = .2
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -12,8 +22,18 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * sens_horizontal)) #converts mousemovement to camera movement
+		visuals.rotate_y(deg_to_rad(event.relative.x * sens_horizontal))
+		camera_mount.rotate_x(deg_to_rad(-event.relative.y * sens_vertical))
 
 func _physics_process(delta: float) -> void:
+	
+	if Input.is_action_pressed("run"):
+		SPEED = running_speed
+		running = true
+	else:
+		SPEED = walking_speed
+		running = false
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -24,13 +44,24 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		if animation_player.current_animation != "walking" and !running:
+			animation_player.play("walking")
+			
+		if animation_player.current_animation != "running" and running:
+			animation_player.play("running")
+		
+		visuals.look_at(position + direction)
+		
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
+		if animation_player.current_animation != "idle":
+			animation_player.play("idle")
+		
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+	if !is_locked:
+		move_and_slide()
