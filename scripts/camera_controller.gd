778a2dc938d07bbca8773641df4ee52ba23e1df7
Caleb Cook <bmxcaleb@gmail.com>
2025-05-19 06:36:53 -0500

    removed some assets to fix errors
    more work on controller v2

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -13,11 +13,19 @@ var pitch_sensitivity : float = 0.7
 var yaw_accelleration : float = 15
 var pitch_accelleration : float = 15
 
+var pitch_max = 60
+var pitch_min = -75
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		yaw += -event.relative.x * yaw_sensitivity
 		pitch += event.relative.y * pitch_sensitivity
 
 func _physics_process(delta: float) -> void:
+	pitch = clamp(pitch, pitch_min, pitch_max)
+	
 	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, yaw_accelleration * delta)
 	pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, pitch_accelleration * delta)
