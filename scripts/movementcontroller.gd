ca26f09d8c3d94bf9129c020b960af0cea449f8e
Caleb "Trip" Cook <68445331+LightningFrisk@users.noreply.github.com>
2025-05-23 18:49:20 -0500

    functioning jump/double jump + animations

--- scripts/movementcontroller.gd
+++ scripts/movementcontroller.gd
@@ -3,6 +3,8 @@ extends Node
 @export var player : CharacterBody3D
 @export var mesh_root : Node3D
 @export var rotation_speed : float = 8
+@export var fall_gravity = 45
+var jump_gravity : float = fall_gravity
 var direction : Vector3
 var velocity : Vector3
 var acceleration : float
@@ -14,12 +16,22 @@ func _physics_process(delta: float) -> void:
 	velocity.x = speed * direction.normalized().x
 	velocity.z = speed * direction.normalized().z
 	
+	if not player.is_on_floor(): #makes the player fall, also allows fall rate to be slower when falling than with jumping
+		if velocity.y >= 0: 
+			velocity.y -= jump_gravity * delta
+		else:
+			velocity.y -= fall_gravity * delta
+	
 	#I'm pretty sure this is going to give the 'weight', linear interpolating speed with the desired speed and acceleration, normalized by delta
 	player.velocity = player.velocity.lerp(velocity, acceleration * delta)
 	player.move_and_slide()
 	#does the same thing except for rotation of the character. This should prevent the snappy movement I had on the previous controller
 	var target_rotation = atan2(direction.x, direction.z) - player.rotation.y
 	mesh_root.rotation.y = lerp_angle(mesh_root.rotation.y, target_rotation, rotation_speed * delta)
+
+func _jump(jump_state: JumpState):
+	velocity.y = 2 * jump_state.jump_height / jump_state.apex_duration
+	jump_gravity = velocity.y /jump_state.apex_duration
 
 #recieve function state and direction from Player
 func _on_set_movement_state(_movement_state : MovementState):
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,16 @@
 extends CharacterBody3D
 
+signal pressed_jump(jump_state:JumpState)
 signal set_movement_state (_movement_state : MovementState)
 signal set_movement_direction(_movement_direction : Vector3)
 
 @export var movement_states: Dictionary
+@export var jump_states : Dictionary
 
 var movement_direction : Vector3
+var air_jump_counter : int = 0
+var max_air_jump: int = 1
+
 
 func _input(event):
 	if event.is_action("movement") or event.is_action_released("movement"): #this function determines movement direction, functions as left + right pressed as neutral
@@ -29,6 +34,21 @@ func _ready(): #by default sets to standing
 func _physics_process(_delta: float) -> void: #emits movement direction
 	if is_movement_ongoing():
 		set_movement_direction.emit(movement_direction)
+		
+	if is_on_floor():
+		air_jump_counter = 0
+	elif air_jump_counter == 0:
+		air_jump_counter = 1
+	
+	if air_jump_counter <= max_air_jump:
+		if Input.is_action_just_pressed("jump"):
+			var jump_name : String = "ground_jump"
+			
+			if air_jump_counter > 0:
+				jump_name = "air_jump"
+				
+			pressed_jump.emit(jump_states[jump_name])
+			air_jump_counter += 1
 
 func is_movement_ongoing() -> bool: #verifies that movement is greater than 0, positive, so no moon walking
 	return abs(movement_direction.x) > 0 or abs(movement_direction.z) > 0
